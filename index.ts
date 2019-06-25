70af8d3d32bf79add1cff449682d4fe7ca90affa
Martin Jurča <martin.jurca@firma.seznam.cz>
2019-06-25 12:48:58 +0200

    returning null for non-intersecting rectangles instead of 0 to distinguish them from 0-sized rectangles

--- index.ts
+++ index.ts
@@ -4,12 +4,12 @@ export = (
 ): null | number => {
   const dx = (x1 < x2) ? x1 + w1 - x2 : x2 + w2 - x1;
   if (dx < 0) {
-    return 0;
+    return null;
   }
 
   const dy = (y1 < y2) ? y1 + h1 - y2 : y2 + h2 - y1;
   if (dy < 0) {
-    return 0;
+    return null;
   }
 
   return dx * dy;
